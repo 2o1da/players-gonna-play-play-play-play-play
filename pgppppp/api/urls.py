1bfdb13bcee8067bec55d747fa7046cb9960b30d
DaSol Kim <solda@khu.ac.kr>
2021-06-14 22:15:52 +0900

    Update the files:React Integration with Webpack & Babel

--- pgppppp/api/urls.py
+++ pgppppp/api/urls.py
@@ -1,11 +1,12 @@
 # 새로 만듦
 from django.urls import path  # , include
-from .views import main
+from .views import RoomView  # main
 
 urlpatterns = [
     # path('',include('api.urls'))
     # api/home
-    path('home', main),
-    # api/
-    path('', main)
+    #     path('home', main),
+    #     # api/
+    #     path('', main)
+    path('room', RoomView.as_view()),
 ]
--- pgppppp/api/views.py
+++ pgppppp/api/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
+# def main(request):
+#     return HttpResponse('<h1>Help</h1><div>When I was younger so much younger then today</div>')
 
 
-def main(request):
-    return HttpResponse('HELP')
+# class RoomView(generics.ListAPIView):
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
