f631b0b043667c9462f0186c059f0b1a7ce2ca0c
DaSol Kim <solda@khu.ac.kr>
2021-06-20 12:55:15 +0900

    Update the file to deploy

--- pgppppp/pgppppp/settings.py
+++ pgppppp/pgppppp/settings.py
@@ -143,6 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
+    '2o1da.pythonanywhere.com',
     'http://localhost:3000', 'https://localhost:3000', 'http://127.0.0.1:8000', 'http://localhost:8000'
 )
 CORS_ALLOWED_ORIGIN_REGEXES = [
