1ad2570412a1a7faeda9561bb735cdc6da8ae734
DaSol Kim <solda@khu.ac.kr>
2021-06-20 12:43:08 +0900

    Update the file to deploy

--- pgppppp/pgppppp/settings.py
+++ pgppppp/pgppppp/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'pgppppp.urls'
 
-SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 TEMPLATES = [
     {
@@ -129,12 +128,12 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "client/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    # 실제 static 파일은 모두 client 측에서 소유
-    os.path.join(BASE_DIR, 'client')
-]
+# STATICFILES_DIRS = [
+#     # 실제 static 파일은 모두 client 측에서 소유
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
